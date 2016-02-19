19b06f58d423b6c7a9048a8604f1932cc7e52f6d
rschwiebert <ryan.c.schwiebert@gmail.com>
2016-02-19 15:23:54 -0500

    Add event list and detail, event-rating list.

--- club/views.py
+++ club/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 import requests
-from club.models import Player, Rating
+from club.models import Player, Rating, Event
 
 # Create your views here.
 def index(request):
     template = loader.get_template('club/index.html')
     context = RequestContext(request,{})
     return HttpResponse(template.render(context))
+
 
 class PlayerDetailView(DetailView):
     model = Player
@@ -19,3 +20,34 @@ class PlayerDetailView(DetailView):
         ratings_list = Rating.objects.filter(player=self.object)
         context['rating_list'] = ratings_list
         return context
+
+
+class EventDetailView(DetailView):
+    model = Event
+
+
+class PlayerListView(ListView):
+    model = Player
+
+
+class EventListView(ListView):
+    model = Event
+
+class EventRatingListView(ListView):
+    model = Rating
+
+    @property
+    def event(self):
+        return Event.objects.get(id=self.kwargs['pk']) or None
+
+    def get_context_data(self):
+        context = super(ListView, self).get_context_data()
+        context['event'] = self.event
+        return context
+
+    def get_queryset(self, **kwargs):
+        current_event = Event.objects.get(id=self.kwargs['pk'])
+        queryset = super(ListView, self).get_queryset(**kwargs)
+        queryset = queryset.filter(event=current_event).order_by('-rating')
+        return queryset
+    
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,12 +5,12 @@ admin.autodiscover()
 
 import club.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
     url(r'^$', club.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^player/(?P<pk>[0-9]+)$', club.views.PlayerDetailView.as_view(), name='player-detail'),
+    url(r'^players/$', club.views.PlayerListView.as_view(), name='player-list'),
+    url(r'^players/(?P<pk>[0-9]+)$', club.views.PlayerDetailView.as_view(), name='player-detail'),
+    url(r'^events/$', club.views.EventListView.as_view(), name='event-list'),
+    url(r'^events/(?P<pk>[0-9]+)$', club.views.EventDetailView.as_view(), name='event-detail'),
+    url(r'^events/(?P<pk>[0-9]+)/ratings$', club.views.EventRatingListView.as_view(), name='event-rating-list'),
 ]
