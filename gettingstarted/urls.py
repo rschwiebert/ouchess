d95b8071746923231b33980d6b7339e2124c5a95
rschwiebert <ryan.c.schwiebert@gmail.com>
2016-02-23 16:21:59 -0500

    Forgot to switch the endpoint urls from events to ladders.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^players/$', club.views.PlayerListView.as_view(), name='player-list'),
     url(r'^players/(?P<pk>[0-9]+)$', club.views.PlayerDetailView.as_view(), name='player-detail'),
-    url(r'^events/$', club.views.LadderListView.as_view(), name='ladder-list'),
-    url(r'^events/(?P<pk>[0-9]+)$', club.views.LadderDetailView.as_view(), name='ladder-detail'),
-    url(r'^events/(?P<pk>[0-9]+)/ratings$', club.views.LadderRatingListView.as_view(), name='ladder-rating-list'),
+    url(r'^ladders/$', club.views.LadderListView.as_view(), name='ladder-list'),
+    url(r'^ladders/(?P<pk>[0-9]+)$', club.views.LadderDetailView.as_view(), name='ladder-detail'),
+    url(r'^ladders/(?P<pk>[0-9]+)/ratings$', club.views.LadderRatingListView.as_view(), name='ladder-rating-list'),
 ]
